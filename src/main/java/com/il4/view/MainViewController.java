477a6b26b5ea0cf202d24b43c90f485fa89738fb
Sébastien Schoepfer <sebastien.schoepfer@icloud.com>
2017-05-03 21:23:44 +0200

    Disable du bouton start après le 1er lancement

--- src/main/java/com/il4/view/MainViewController.java
+++ src/main/java/com/il4/view/MainViewController.java
@@ -7,10 +7,7 @@ import com.il4.view.component.OuvrierView;
 import com.il4.view.component.TransporteurView;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.ListView;
-import javafx.scene.control.Spinner;
-import javafx.scene.control.SpinnerValueFactory;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.VBox;
 
 import java.net.URL;
@@ -31,6 +28,8 @@ public class MainViewController implements Initializable {
     @FXML public TextField textFieldOuvrierName;
 
     @FXML public Spinner<Integer> spinnerTotalBenneToFill;
+
+    @FXML public Button buttonStart;
 
     @FXML public ListView<String> ListTransporteurWaitingBenneBucheron;
     @FXML public ListView<String> ListTransporteurWaitingBenneOuvrier;
@@ -100,6 +99,7 @@ public class MainViewController implements Initializable {
     public void buttonStartClick(){
         BackgroundApplication.getInstance().setBenneToFillCount(spinnerTotalBenneToFill.getValue());
         BackgroundApplication.getInstance().Start();
+        buttonStart.setDisable(true);
     }
 
     @FXML  public void buttonAddBucheron(){
