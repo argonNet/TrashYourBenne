8ace188a7b09b4c78106f0185898740bd31be40e
argon <sebastien.schoepfer@gmail.com>
2017-06-11 21:37:57 +0200

    Comment pourir les commits

---
+++ src/main/java/com/il4/view/component/WorkerView.java
@@ -0,0 +1,14 @@
+package com.il4.view.component;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+/**
+ * Created by Argon on 11.06.17.
+ */
+public class WorkerView extends ActeurView{
+    @FXML
+    public Label labelWorkerType;
+
+
+}
